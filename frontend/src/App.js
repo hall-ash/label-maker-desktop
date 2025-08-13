16398fc2b76bbc6196d541acfd50fedba0826521
ahall <ahall@inventprise.com>
2025-08-13 12:20:52 -0700

    delete unused import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
